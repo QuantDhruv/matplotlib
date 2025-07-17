9baf7bb5e4070c0c4ec730ebbcd5216ae88c08f6
Dhruv Yadav <155057260+QuantDhruv@users.noreply.github.com>
2025-07-17 23:45:48 +0530

    basic  Scatter Plot

---
+++ lecture3/main.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [5, 4, 3, 2, 1]
+
+plt.scatter(x, y, color='red')
+plt.title('Scatter Plot Example')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
